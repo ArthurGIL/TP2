733ca3dee0ae072c93b738073cace3a946ba8a7b
ArthurGIL <49671280+ArthurGIL@users.noreply.github.com>
2020-03-29 23:12:02 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Graphe.h"
+
+void afficherParcours(size_t s0, const std::vector<int>& precesseur) {
+    for(size_t i = 0; i < precesseur.size(); ++i) {
+        if (i != s0) {
+            if(precesseur[i] != -1) {
+                std::cout << i << " <--- ";
+                size_t j = precesseur[i];
+                while(j != s0) {
+                    std::cout << j << " <--- ";
+                    j = precesseur[j];
+                }
+                std::cout << j << std::endl;
+            }
+        }
+    }
+}
+
+int main() {
+    size_t s0 = 0;
+    Graphe g{"C://Users/Arthur/CLionProjects/TP2/graphe-no-1.txt"};
+    std::vector<int> arborescence;
+    g.afficher();
+    std::cout << std::endl << "BFS : Veuillez saisir le numéro du sommet initial pour la recherche du plus court chemin : ";
+    std::cin >> s0;
+    arborescence = g.BFS(s0);
+    std::cout << "Plus courts chemins depuis le sommet " << s0 << " (BFS) : " << std::endl;
+    afficherParcours(s0, arborescence);
+    std::cout << std::endl <<std::endl << "DFS : Veuillez saisir le numéro du sommet initial pour la recherche du plus court chemin : ";
+    std::cin >> s0;
+    arborescence = g.DFS(s0);
+    std::cout << "Plus courts chemins depuis le sommet " << s0 << " (DFS) : " << std::endl;
+    afficherParcours(s0, arborescence);
+    return 0;
+}
